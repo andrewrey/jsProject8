9f5fd97282076bd762e0d737f035d3326085cd0d
Andrew Reynolds <andrew_reynolds@mac.com>
2020-09-04 13:24:44 -0700

    completed random meal fetch

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ const search = document.getElementById('search'),
 
 // Functions // 
 
+
 // Search Meal and fetch from API
 function searchMeal(e){
   e.preventDefault();
@@ -56,6 +57,19 @@ function getMealByID(idNum){
         });
 }
 
+// Fetch a random meal from API
+function getRandomMeal(){
+  // Clear meals and heading
+  mealsEl.innerHTML = '';
+  resultHeading.innerHTML = '';
+
+  fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(data => {
+      const meal = data.meals[0];
+      addMealToDOM(meal);
+    });
+}
 
 // Add meal to DOM
 function addMealToDOM(meal){
@@ -88,6 +102,8 @@ function addMealToDOM(meal){
 
 // Event Listeners
 submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', getRandomMeal);
+
 
 mealsEl.addEventListener('click', (e)=>{
   const mealInfo = e.path.find(item => {
