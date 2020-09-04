0ac83d7454fd0d3dbc7747c77f14285d09459ca2
Andrew Reynolds <andrew_reynolds@mac.com>
2020-09-03 17:45:53 -0700

    completed fetch meal with id and print meal info to screen

--- js/app.js
+++ js/app.js
@@ -46,6 +46,63 @@ function searchMeal(e){
   }
 }
 
+// Fetch meal info from meal ID number
+function getMealByID(idNum){
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idNum}`)
+        .then(res => res.json())
+        .then(data => {
+          const meal = data.meals[0];
+          addMealToDOM(meal);
+        });
+}
+
+
+// Add meal to DOM
+function addMealToDOM(meal){
+  const ingredients = [];
+  for(let i = 1; i <= 20; i++){
+    if(meal[`strIngredient${i}`]){
+      ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+    } else {
+      break;
+    }
+  }
+  single_mealEl.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="single-meal-info">
+        ${meal.strCategory? `<p>${meal.strCategory}</p>`: ''}
+        ${meal.strArea ?`<p>${meal.strArea}</p>`: ''}
+      </div>
+      <div class="main">
+        <p>${meal.strInstructions}</p>
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+        </ul>
+      </div>
+    </div>
+  `;
+}
 
 // Event Listeners
 submit.addEventListener('submit', searchMeal);
+
+mealsEl.addEventListener('click', (e)=>{
+  const mealInfo = e.path.find(item => {
+    if(item.classList){
+      return item.classList.contains('meal-info');
+    } else {
+      return false;
+    }
+  })
+  // if(mealInfo){
+  //   const mealID = mealInfo.dataset.mealid;
+  //   return mealID;
+  // }
+  if(mealInfo){
+    let mealID = mealInfo.dataset.mealid;
+    getMealByID(mealID);
+  }
+});
